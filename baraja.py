e2c75c8f83325fb525b91d8fd7f6063ecf2ebe96
Alberto Alegre Bravo <albertoalegrebravo@MacBook-Pro-de-Alberto-2.local>
2020-11-11 00:27:04 +0100

    segunda version programa baraja

--- baraja.py
+++ baraja.py
@@ -1,13 +1,21 @@
 numeros = ["A", "2", "3", "4", "5", "6", "7", "S", "C", "R"]
 palos = ["o", "c", "e", "b"]
 
-baraja = []
-
 def creaBaraja():
-
+    baraja = []
     for palo in palos:
         for numero in numeros:
             baraja.append(numero + palo)
     return baraja
 
-print(baraja)
+
+def creaJugador():
+    newPlayers = []
+    for i in range(3):
+        newPlayers.append("NewPlayer" + str(i))
+    return newPlayers
+
+
+jugadores = creaJugador()
+print(jugadores)
+
