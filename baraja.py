783a4447a3f20351cea44b7612598388defae838
Alberto Alegre Bravo <albertoalegrebravo@MacBook-Pro-de-Alberto-2.local>
2020-11-11 22:32:50 +0100

    Funcion barajar

--- baraja.py
+++ baraja.py
@@ -1,3 +1,5 @@
+import random
+
 numeros = ["A", "2", "3", "4", "5", "6", "7", "S", "C", "R"]
 palos = ["o", "c", "e", "b"]
 
@@ -8,7 +10,38 @@ def creaBaraja():
             baraja.append(numero + palo)
     return baraja
 
+def barajar():
+    baraja = creaBaraja()
+    for i in range(40):
+        if baraja[i] != baraja[i]:
+            baraja = baraja.append(baraja[i])
+        else:
+            pass
+    return baraja
 
+
+prueba = barajar()
+print(prueba)
+
+
+'''        
+    baraja = random.choices(baraja, k=40)
+
+    return baraja
+
+
+cartasBarajadas = barajar(creaBaraja)
+print(cartasBarajadas)
+
+    
+
+cartasMovidas = barajar(creaBaraja)
+
+print(cartasMovidas)'''
+
+
+
+'''
 def creaJugador():
     newPlayers = []
     for i in range(3):
@@ -17,5 +50,6 @@ def creaJugador():
 
 
 jugadores = creaJugador()
-print(jugadores)
+baraja = creaBaraja()
 
+print("El jugador {} tiene la carta: {}".format(random.choice(jugadores), random.choice(baraja)))'''
